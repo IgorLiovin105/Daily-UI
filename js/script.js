d1559605a757fbd61a3c442cca7a5fae30775504
IgorLiovin105 <igorliovin07@gmail.com>
2021-12-29 10:46:24 +0600

    header

--- js/script.js
+++ js/script.js
@@ -1,32 +1,9 @@
-function zeroFirstFormat(value) {
-    if (value < 10) {
-        value = '0' + value
-    }
-    return value
+const dashboardUser = document.querySelector('.dashboard__user')
+const dashboardCustomize = document.querySelector('.dashboard__customize')
+
+if(dashboardUser) {
+    dashboardUser.addEventListener('click', function() {
+        dashboardUser.classList.toggle('_active')
+        dashboardCustomize.classList.toggle('_active')
+    })
 }
-
-    /* функция получения текущей даты и времени */
-
-function currentTime() {
-    let currentTime = new Date();
-    let hours = zeroFirstFormat(currentTime.getHours())
-    let minutes = zeroFirstFormat(currentTime.getMinutes())
-    let seconds = zeroFirstFormat(currentTime.getSeconds())
-    return hours + ':' + minutes + ':' + seconds
-}
-
-document.querySelector('.header__time').innerHTML = currentTime()
-
-setInterval(function () {
-    document.querySelector('.header__time').innerHTML = currentTime()
-}, 1000)
-
-function currentDate() {
-    let currentDate = new Date()
-    let day = zeroFirstFormat(currentDate.getDate())
-    let month = zeroFirstFormat(currentDate.getMonth()+1)
-    let year = currentDate.getFullYear()
-    return day + '.' + month + '.' + year
-}
-
-document.querySelector('.header__date').innerHTML = currentDate()
